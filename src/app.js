5fc6656e632617fe77489b51568597241a0116e6
Matheus Maurici <mauricimatheus@gmail.com>
2020-05-18 12:51:28 -0300

    Projeto put, post, delete, get com Links

--- src/app.js
+++ src/app.js
@@ -39,9 +39,7 @@ app.put("/repositories/:id", (request, response) => {
       techs,
       likes: repositories[repositoryIndex].likes,
   };
-
     repositories[repositoryIndex] = repository;
-
     return response.json(repository);
 });
 
@@ -49,13 +47,11 @@ app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
   if (repositoryIndex < 0) {
     return response.status(400).json({ error: 'Repository not found. '})
 }
 
   repositories.splice(repositoryIndex, 1);
-
   return response.status(204).send();
 });
 
@@ -63,7 +59,6 @@ app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
   
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
   if (repositoryIndex === -1) {
     return response.status(400).json({ error: 'Repository does not exists. '});
   }
